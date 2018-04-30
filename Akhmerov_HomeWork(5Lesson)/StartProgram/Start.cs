ff776a1ee7951e18831aacb3e6a2056e8cd7942c
Zerohout <zerohout@gmail.com>
2018-04-30 22:21:59 +0300

    Create class StartProgram and it's methods

---
+++ Akhmerov_HomeWork(5Lesson)/StartProgram/Start.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sepo;
+using Task_1;
+
+namespace StartProgram
+{
+    class Start
+    {
+        private static bool exit;
+        static void Main()
+        {
+            while(true){
+
+                Console.Clear();
+                var st = new SetLabel();
+                st.Label("Выберите действие");
+                st.AddPoint(0, "Выход");
+                st.AddPoint(1, "Проверка ввода логина");
+
+
+                switch (st.UserSelTask())
+                {
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        CheckLogin.Main();
+                        break;
+                }
+
+                if (exit)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
