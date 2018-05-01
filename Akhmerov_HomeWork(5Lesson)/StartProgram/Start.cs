c34ef5e1690d31a33fafcc356b8c584d8aaaa299
Zerohout <zerohout@gmail.com>
2018-05-01 15:44:09 +0300

    Add Class Task_2 and class MyString

--- Akhmerov_HomeWork(5Lesson)/StartProgram/Start.cs
+++ Akhmerov_HomeWork(5Lesson)/StartProgram/Start.cs
@@ -3,6 +3,7 @@
     using System;
     using Sepo;
     using Task_1;
+    using Task_2;
 
     class Start
     {
@@ -24,6 +25,7 @@
             st.Label("Выберите действие");
             st.AddPoint(0, "Выход");
             st.AddPoint(1, "Проверка ввода логина");
+            st.AddPoint(2,"Операции с текстом");
 
             switch (st.UserSelTask())
             {
@@ -32,6 +34,9 @@
                     return false;
                 case 1:
                     CheckLogin.Main();
+                    return true;
+                case 2:
+                    Task_2.Main();
                     return true;
                 default:
                     return true;
